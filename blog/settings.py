deea07cebb2f34bf59d3c8edb30dfda20995ba7e
Kumar Shubham <shubhamstudent5@gmail.com>
2020-08-05 14:30:20 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -134,6 +134,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shubhamstudent5@gmail.com'
-EMAIL_HOST_PASSWORD = 'ardazpkbdapmeclj'
+EMAIL_HOST_USER = '**************'
+EMAIL_HOST_PASSWORD = '************'
 
